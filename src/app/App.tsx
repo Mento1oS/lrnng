52a170a4f1f0766053932f6eca962b2899f017f2
Artem_Mentolos <mentolosscg@yandex.ru>
2024-09-14 12:56:01 +0300

    Merge remote-tracking branch 'origin/dev-t' into dev-t

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/ClassNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
     const {toggleTheme, theme} = useTheme()
     return <div className={classNames('app', {}, [theme])}>
+        <Navbar/>
+        <AppRouter/>
         <div>
             <button onClick={toggleTheme}>Switch theme</button>
         </div>
-        <Link to={'/'}>Main</Link>
-        <Link to={'/about'}>About</Link>
-        <AppRouter/>
     </div>
 }
 
--- src/shared/lib/ClassNames/classNames.ts
+++ src/shared/lib/ClassNames/classNames.ts
@@ -1,5 +1,5 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
-    return [cls, ...Object.entries(mods).filter(([cls, flag]) => Boolean(flag)).map(([cls, elem]) => cls), ...additional].join(' ');
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+    return [cls, ...Object.entries(mods).filter(([cls, flag]) => Boolean(flag)).map(([cls, elem]) => cls), ...additional.filter(Boolean)].join(' ');
 }

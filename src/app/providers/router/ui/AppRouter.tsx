4cf54bccbfe9edf66a68daceefd08f01260f4a91
Artem_Mentolos <mentolosscg@yandex.ru>
2024-09-16 13:21:05 +0300

    scss linting added

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,15 +1,19 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { useTranslation } from 'react-i18next';
 
-const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ path, element }) => (
-                <Route path={path} element={<div className="page-wrapper">{element}</div>} key={path} />
-            ))}
-        </Routes>
-    </Suspense>
-);
+const AppRouter = () => {
+    const { t } = useTranslation();
+    return (
+        <Suspense fallback={<div className="page-loader">{t('Loading...')}</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({ path, element }) => (
+                    <Route path={path} element={<div className="page-wrapper">{element}</div>} key={path} />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
 
 export default AppRouter;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { classNames } from 'shared/lib/ClassNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
 import * as cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -13,8 +14,8 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>{t('Main')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('About')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath.main} className={cls.mainLink}>{t('Main')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath.about}>{t('About')}</AppLink>
             </div>
         </div>
     );
